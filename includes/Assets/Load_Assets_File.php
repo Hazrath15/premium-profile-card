0c77ac64f5693105ed649e6c047995b12636624e
hazrath15 <mohammadhazrathalicse15@gmail.com>
2025-07-27 14:44:08 +0600

    load assets

--- includes/Assets/Load_Assets_File.php
+++ includes/Assets/Load_Assets_File.php
@@ -8,8 +8,8 @@ class Load_Assets_File {
     }
 
     public function prpc_enqueue_scripts() {
-        wp_enqueue_style( 'premium-profile-card-style', plugins_url( '../../assets/css/main.css', __FILE__ ), [], null );
-        // wp_enqueue_script( 'premium-profile-card-script', plugins_url( '../assets/js/script.js', __FILE__ ), [ 'jquery' ], null, true );
+        wp_enqueue_style( 'premium-profile-card-style', plugins_url( '../../assets/css/main.css', __FILE__ ), [], '1.0.0' );
+        wp_enqueue_script( 'premium-profile-card-script', plugins_url( '../../assets/js/main.js', __FILE__ ), [ 'jquery' ], '1.0.0', true );
     }
 }
 ?>
